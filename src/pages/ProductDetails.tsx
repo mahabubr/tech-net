42628ed0ca295c3cd09c3ed8ed7ab513a85335ed
mahabubr <mahabub.r753@gmail.com>
2023-07-10 04:08:14 +0600

    done redux

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,6 +1,6 @@
 import ProductReview from '@/components/ProductReview';
 import { Button } from '@/components/ui/button';
-import { useGetSingleProductQuery } from '@/redux/api/apiSlice';
+import { useGetSingleProductQuery } from '@/redux/features/products/productApi';
 import { IProduct } from '@/types/globalTypes';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -27,7 +27,7 @@ export default function ProductDetails() {
           <Button>Add to cart</Button>
         </div>
       </div>
-      <ProductReview />
+      <ProductReview id={id!} />
     </>
   );
 }
